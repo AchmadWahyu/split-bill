27c7bb13b757a31e5ac3a54322be9cb74c7118c5
AchmadWahyu <achwahyu.w@gmail.com>
2025-05-12 17:05:11 +0700

    chore: remove unused files

--- src/routes/Form2 copy.tsx
+++
@@ -1,8 +0,0 @@
-export const Form3 = () => {
-  return (
-    <div>
-      <h1>Form 3</h1>
-      <p>This is Form 3</p>
-    </div>
-  );
-};
--- src/routes/Form2.tsx
+++
@@ -1,8 +0,0 @@
-export const Form2 = () => {
-  return (
-    <div>
-      <h1>Form 2</h1>
-      <p>This is Form 2</p>
-    </div>
-  );
-};
