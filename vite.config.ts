27d371362df972f41a45041e9172fcacfddff1a5
AchmadWahyu <achwahyu.w@gmail.com>
2025-05-18 11:40:53 +0700

    chore: update sentry project name

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react(), tailwindcss(), sentryVitePlugin({
     org: "achwahyu",
-    project: "javascript-react"
+    project: "split-bill"
   })],
 
   resolve: {
