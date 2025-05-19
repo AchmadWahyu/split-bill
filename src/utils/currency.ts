952316cf7dba1e04254d4a125c3fee1e0c1a3b5a
AchmadWahyu <achwahyu.w@gmail.com>
2025-05-19 20:10:08 +0700

    fix: operations number miss and update fraction digits to 0

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -2,6 +2,6 @@ export const formatCurrencyIDR = (price: number) => {
   return new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
-    maximumFractionDigits: 2,
+    maximumFractionDigits: 0,
   }).format(price);
 };
