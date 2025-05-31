f9222af4627c9acc0d1f44de7a4c6413d97a4002
AchmadWahyu <achwahyu.w@gmail.com>
2025-05-31 12:30:00 +0700

    fix: back to home button not reloading page

--- src/routes/CustomErrorPage.tsx
+++ src/routes/CustomErrorPage.tsx
@@ -1,26 +1,20 @@
 import { Button } from '@/components/ui/button';
-import { useNavigate } from 'react-router';
 
-const CustomErrorPage = () => {
-  const navigate = useNavigate();
-
-  return (
-    <main className="p-8">
-      <div className="h-dvh flex flex-col items-center justify-center text-center">
-        <h1 className="mt-4 text-xl">😕 Ada yang salah...</h1>
-        <p className="mt-2 text-slate-600">
-          Sepertinya aplikasi mengalami kendala saat memuat data tagihan kamu.
-        </p>
-        <Button
-          onClick={() => navigate('/')}
-          type="button"
-          className="bg-primary hover:bg-primary-variant hover:bg-slate-800 text-white h-12 whitespace-normal mt-6"
-        >
-          Balik ke Home
-        </Button>
-      </div>
-    </main>
-  );
-};
-
+const CustomErrorPage = () => (
+  <main className="p-8">
+    <div className="h-dvh flex flex-col items-center justify-center text-center">
+      <h1 className="mt-4 text-xl">😕 Ada yang salah...</h1>
+      <p className="mt-2 text-slate-600">
+        Sepertinya aplikasi mengalami kendala saat memuat data tagihan kamu.
+      </p>
+      <Button
+        onClick={() => location.replace('/')}
+        type="button"
+        className="bg-primary hover:bg-primary-variant hover:bg-slate-800 text-white h-12 whitespace-normal mt-6"
+      >
+        Balik ke Home
+      </Button>
+    </div>
+  </main>
+);
 export default CustomErrorPage;
