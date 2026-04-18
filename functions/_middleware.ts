de21e2365f70d70d17ae404eb6188030f919f3b6
Achmad Wahyu <achwahyu.w@gmail.com>
2026-04-18 17:08:10 +0700

    chore: update env var

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,9 +2,9 @@ import * as Sentry from '@sentry/cloudflare';
 
 export const onRequest = [
   Sentry.sentryPagesPlugin((context) => ({
-    dsn: 'https://868b22f34ebc3f638749df6cdf5420f3@o4508209070342144.ingest.us.sentry.io/4509342217076736',
-    sendDefaultPii: true,
-    enableLogs: true,
-    tracesSampleRate: 1.0,
+    dsn: process.env.SENTRY_DSN,
+    sendDefaultPii: false,
+    enableLogs: process.env.NODE_ENV !== 'production',
+    tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
   })),
 ];
